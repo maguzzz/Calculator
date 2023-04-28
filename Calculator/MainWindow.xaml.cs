976cab91eb2c52a14293bbd1059bbe72e1e22cb0
Maguzzz <markusm.touray@gmail.com>
2023-04-28 11:25:32 +0200

    Basic calc possible

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -44,9 +44,92 @@ namespace Calculator
             Application.Current.Shutdown();
         }
 
+        string userInput;
+        string lastUserInput;
+
+        //Calculator
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Get the value of the clicked butto
+            var clickedButton = sender as Button;
 
+            // Puting the value into a string
+            string buttonValue = Convert.ToString(clickedButton.Content);
+
+            //Chcking wich button got pressed
+            switch (buttonValue)
+            {
+                case "del":
+                    //Delete - 1
+                    break;
+                case "%":
+                    userInput += "%";
+                    break;
+                case "/":
+                    userInput += "/";
+                    break;
+                case "7":
+                    userInput += "7";
+                    break;
+                case "8":
+                    userInput += "8";
+                    break;
+                case "9":
+                    userInput += "9";
+                    break;
+                case "*":
+                    userInput += "*";
+                    break;
+                case "4":
+                    userInput += "4";
+                    break;
+                case "5":
+                    userInput += "5";
+                    break;
+                case "6":
+                    userInput += "6";
+                    break;
+                case "-":
+                    userInput += "-";
+                    break;
+                case "1":
+                    userInput += "1";
+                    break;
+                case "2":
+                    userInput += "2";
+                    break;
+                case "3":
+                    userInput += "3";
+                    break;
+                case "+":
+                    userInput += "+";
+                    break;
+                case "?":
+                    userInput += "?";
+                    break;
+                case "0":
+                    userInput += "=";
+                    break;
+                case ".":
+                    userInput += ":";
+                    break;
+                case "=":
+
+                    //Adding a plus after the last value so it can be a  calculation
+                    userInput += "0";
+
+                    //Convertin it to a calculation
+                    userInput = Convert.ToString(new System.Data.DataTable().Compute(userInput, ""));
+
+                    break;
+                default:
+                    userInput = "ERROR";
+                    break;
+
+            }
+            // Displaying the Text on the UI
+            OutputText.Text = userInput;
         }
     }
 }
+
