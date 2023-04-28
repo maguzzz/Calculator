927b9cc2f3f333b7ed8458e966918f84182ce4b2
Albin <Albin@DESKTOP-2LCI1U3>
2023-04-28 11:17:09 +0200

    design almost

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -25,6 +25,15 @@ namespace Calculator
             InitializeComponent();
         }
         //This is a commend
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
@@ -32,7 +41,7 @@ namespace Calculator
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.Shutdown();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
